6041aac25f0f768f7ea45c520a04f903e86a6f0a
onlybenyang <thomas199388@163.com>
2017-12-06 00:08:40 +0800

    start to learn how to build decision tree recursively

--- DecsionTree.py
+++ DecsionTree.py
@@ -2,6 +2,7 @@
 #决策树
 #----
 from math import log
+import operator
 
 
 #---信息熵---
@@ -62,10 +63,46 @@ def chooseBestFeatureToSplit(dataSet):
     return bestFeature
 
 
-if __name__  == '__main__':
-    dataSet, labels = createDataSet()
-    #newdataSet = splitDataSet(dataSet, 0, 0)
+#---递归构建决策树---#
+
+#----------------------------------------------------------------------
+def marjorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vate not in classCount.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems, key=operator.itemgetter[1], reverse=True)
+    return sortedClassCount
+    
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    #print(classList)
+    #print(classList.count(classList[0]))
+    #如果类别完全相同就停止划分
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return marjorityCnt(classList)
     bestFeature = chooseBestFeatureToSplit(dataSet)
-    print(bestFeature)
-    #infoEntropy = getInfoEntropy(dataSet)
-    #print(infoEntropy)
+    bestFeatureLabel = labels[bestFeature]
+    myTree = {bestFeatureLabel: {}}
+    del(labels[bestFeature])
+    featValues = [example[bestFeature] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatureLabel][value] = createTree(splitDataSet(dataSet, bestFeature, value), subLabels)
+    return myTree
+    
+
+
+if __name__  == '__main__':
+    #--- 获得最佳划分数据集的特征
+    dataSet, labels = createDataSet()
+    bestFeature = chooseBestFeatureToSplit(dataSet)
+    #print(bestFeature)
+    
+    #---递归创建决策树
+    tree = createTree(dataSet, labels)
+    print(tree)
